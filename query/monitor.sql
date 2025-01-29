a6ee13bedf44967ecc654e9a3bfa431139e038a0
Vegard Bjørsvik <vegardbjr@gmail.com>
2025-01-29 21:58:22 +0100

    Revert "refactor: specify explicit columns in SQL queries"
    This reverts commit c578f77bfa2ec44e7a3b0dd06b098b70ce7ef191.

--- query/monitor.sql
+++ query/monitor.sql
@@ -1,15 +1,15 @@
 -- name: CreateMonitor :one
 INSERT INTO monitor(user_id, url)
 VALUES ($1, $2)
-RETURNING id, user_id, url, created_at, updated_at;
+RETURNING *;
 
 -- name: GetMonitorsByUserID :many
-SELECT id, user_id, url, created_at, updated_at
+SELECT *
 FROM monitor
 WHERE user_id = $1;
 
 -- name: GetMonitorByUserIDAndMonitorID :one
-SELECT id, user_id, url, created_at, updated_at
+SELECT *
 FROM monitor
 WHERE user_id = $1
   and id = $2;
